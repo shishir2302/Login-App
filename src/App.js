ce89ca6358a50dda193e1853b33deee932a5896e
Shishir singh <108649653+shishir2302@users.noreply.github.com>
2023-11-06 13:46:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+/** import all components */
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+
+
+
+/** root routes */
+const router = createBrowserRouter([
+    {
+        path : '/',
+        element : <Username></Username>
+    },
+    {
+        path : '/register',
+        element : <Register></Register>
+    },
+    {
+        path : '/password',
+        element : <Password></Password>
+    },
+    {
+        path : '/reset',
+        element : <Reset></Reset>
+    },
+    {
+        path : '*',
+        element : <PageNotFound></PageNotFound>
+    },
+])
+
+export default function App() {
+  return (
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
+  )
+}
